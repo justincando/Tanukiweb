6905ee074a48b174fc632457e6c776584c96b39b
JustinSmith <steelindian66@gmail.com>
2022-06-09 12:25:10 -0500

    fixed res positioning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,106 +6,107 @@ import './Navbar.css';
 export default function Navbar(){
     return(
         <nav>
-            <div id = "topNav">
-                <img src = {logo} alt ="tanuki" id = "logo" />
-                <span id ="title">TANUKI toys & collectibles, LLC</span>
-                <input type={"search"} id = "search" placeholder="Search"/>
-                <img src = {search} alt = "search" id = "mag" />
-                <span id = "signin">
-                    <img src = {person} alt ="person" id = "person" />
-                    <span id="login">Sign in</span>
-                </span>
-                <span id = "checkout">
-                    <img src={cart} alt="cart" id ="cart"/>
-                    <span id = "basket">Cart</span>
-                </span>
-            </div>
-            <div id = "bottomNav">
-                <p id = "new">New Additions</p>
-                <p id="restock">Newly Restocked</p>
-                <p id ="theme">Themes</p>
-                    <div id ="drops">
-                        <div id ="themDrop">
+            <div className= "navbar">
+                <div id = "topNav">
+                    <img src = {logo} alt ="tanuki" id = "logo" />
+                    <span id ="title">TANUKI toys & collectibles, LLC</span>
+                    <input type={"search"} id = "search" placeholder="Search"/>
+                    <img src = {search} alt = "search" id = "mag" />
+                    <span id = "signin">
+                        <img src = {person} alt ="person" id = "person" />
+                        <span id="login">Sign in</span>
+                    </span>
+                    <span id = "checkout">
+                        <img src={cart} alt="cart" id ="cart"/>
+                        <span id = "basket">Cart</span>
+                    </span>
+                </div>
+                <div id = "bottomNav">
+                    <p id = "new">New Additions</p>
+                    <p id="restock">Newly Restocked</p>
+                    <p id ="theme">Themes</p>
+                        <div id ="drops">
+                            <div id ="themDrop">
+                                <table>
+                                    <tr>
+                                        <td>Yu yu hakusho</td>
+                                        <td>Dragon Ball Z</td>
+                                        <td>Sailor Moon</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Bleach</td>
+                                        <td>Naruto</td>
+                                        <td>OnePiece</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Full Metal Alchemist</td>
+                                        <td>Hunter X Hunter</td>
+                                        <td>Attack on titan</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Black Clover</td>
+                                        <td>My hero acadamia</td>
+                                        <td>Demon Slayer</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    <p id ="type">Types of Product</p>
+                        <div id ="dropstype">
                             <table>
                                 <tr>
-                                    <td>Yu yu hakusho</td>
-                                    <td>Dragon Ball Z</td>
-                                    <td>Sailor Moon</td>
+                                    <td>Books</td>
+                                    <td>Games</td>
+                                    <td>Art</td>
                                 </tr>
                                 <tr>
-                                    <td>Bleach</td>
-                                    <td>Naruto</td>
-                                    <td>OnePiece</td>
+                                    <td>Figures</td>
+                                    <td>Apparel</td>
+                                    <td>Movies</td>
                                 </tr>
                                 <tr>
-                                    <td>Full Metal Alchemist</td>
-                                    <td>Hunter X Hunter</td>
-                                    <td>Attack on titan</td>
-                                </tr>
-                                <tr>
-                                    <td>Black Clover</td>
-                                    <td>My hero acadamia</td>
-                                    <td>Demon Slayer</td>
+                                    <td>Plushies</td>
+                                    <td>Pins</td>
+                                    <td>Back to school</td>
                                 </tr>
                             </table>
                         </div>
-                    </div>
-                <p id ="type">Types of Product</p>
-                    <div id ="dropstype">
-                        <table>
-                            <tr>
-                                <td>Books</td>
-                                <td>Games</td>
-                                <td>Art</td>
-                            </tr>
-                            <tr>
-                                <td>Figures</td>
-                                <td>Apparel</td>
-                                <td>Movies</td>
-                            </tr>
-                            <tr>
-                                <td>Plushies</td>
-                                <td>Pins</td>
-                                <td>Back to school</td>
-                            </tr>
-                        </table>
-                    </div>
-                <p id ="companies"> Companies</p>
-                    <div id ="dropscomp">
-                        <table>
-                            <tr>
-                                <td>GoodSmile</td>
-                                <td>Max Factory</td>
-                                <td>Phat!</td>
-                                <td>Orange Rouge</td>
-                                
-                            </tr>
-                            <tr>
-                                <td>MegaHouse</td>
-                                <td>Alter</td>
-                                <td>Bioworld</td>
-                                <td>Ques Q</td>
-                                
-                            </tr>
-                            <tr>
-                                <td>Kotobukiya</td>
-                                <td>Banpresto</td>
-                                <td>FREEing</td>
-                                <td>Stronger</td>
-                                
-                            </tr>
-                            <tr>
-                                <td>Pulchra</td>
-                                <td>Nekowear</td>
-                                <td>Aniplex</td>
-                            </tr>
-                        
-                        </table>
-                    </div>
-                <p id = "deals">Deals</p>
-                <p id ="instock">Currently instock</p>
+                    <p id ="companies"> Companies</p>
+                        <div id ="dropscomp">
+                            <table>
+                                <tr>
+                                    <td>GoodSmile</td>
+                                    <td>Max Factory</td>
+                                    <td>Phat!</td>
+                                    <td>Orange Rouge</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>MegaHouse</td>
+                                    <td>Alter</td>
+                                    <td>Bioworld</td>
+                                    <td>Ques Q</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>Kotobukiya</td>
+                                    <td>Banpresto</td>
+                                    <td>FREEing</td>
+                                    <td>Stronger</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>Pulchra</td>
+                                    <td>Nekowear</td>
+                                    <td>Aniplex</td>
+                                </tr>
+                            
+                            </table>
+                        </div>
+                    <p id = "deals">Deals</p>
+                    <p id ="instock">Currently instock</p>
+                </div>
             </div>
-            
         </nav>
     )
 }
